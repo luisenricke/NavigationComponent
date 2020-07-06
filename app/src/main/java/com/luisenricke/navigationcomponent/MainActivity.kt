8734a6c3b04fbd7e4a6c0af373c7bf0be1846803
Luis Villalobos <luisevm24@outlook.com>
2020-07-06 00:47:56 -0500

    add feat to navigate with NavigationView

--- app/src/main/java/com/luisenricke/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/luisenricke/navigationcomponent/MainActivity.kt
@@ -1,13 +1,96 @@
 package com.luisenricke.navigationcomponent
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener {
+
+    private val whichStack: Stack = Stack.SEQUENCE
+    private lateinit var navController: NavController
+    private lateinit var appBarConfig: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+
+        navController = (supportFragmentManager
+            .findFragmentById(R.id.fragment_host) as NavHostFragment)
+            .navController
+
+        appBarConfig = AppBarConfiguration(
+            // navController.graph, drawer
+            setOf(R.id.fragment_first, R.id.fragment_second, R.id.fragment_third), drawer
+        )
+
+        setupActionBarWithNavController(this, navController, appBarConfig)
+        navigation.setupWithNavController(navController)
+        navigation.setNavigationItemSelectedListener(this)
+
+        this.takeIf { savedInstanceState == null }.also {
+            navigation.menu.findItem(R.id.menu_first).isChecked = true
+        }
     }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        item.isChecked = true
+        drawer.closeDrawers()
+
+        if (whichStack == Stack.HOME) {
+            navController.apply { popBackStack(R.id.fragment_first, false) }
+                .takeIf { item.itemId == R.id.menu_first }
+                .also { it?.popBackStack() }
+        }
+
+        when (item.itemId) {
+            R.id.menu_first -> navController.navigate(R.id.fragment_first)
+            R.id.menu_second -> navController.navigate(R.id.fragment_second)
+            R.id.menu_third -> navController.navigate(R.id.fragment_third)
+            else -> return super.onOptionsItemSelected(item)
+        }
+
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+
+            val idSelected = when (navController.currentDestination?.id) {
+                R.id.fragment_first -> navigation.menu.findItem(R.id.menu_first).itemId
+                R.id.fragment_second -> navigation.menu.findItem(R.id.menu_second).itemId
+                R.id.fragment_third -> navigation.menu.findItem(R.id.menu_third).itemId
+                null -> 0.also { Timber.d("Empty stack") }
+                else -> (-1).also { Timber.e("Item doesn't find") }
+            }
+
+            navigation.menu.findItem(idSelected).isChecked = true
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
+    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+//    }
+
+    enum class Stack { HOME, SEQUENCE }
 }
